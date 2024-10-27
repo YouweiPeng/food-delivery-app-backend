4a17a2c075e1cef65463854156398b99dda6eb9e
YouweiPeng <98921666+YouweiPeng@users.noreply.github.com>
2024-10-27 17:14:19 -0600

    admin css

--- food_delivery_app/settings.py
+++ food_delivery_app/settings.py
@@ -71,6 +71,7 @@ REST_FRAMEWORK = {
     ]
 }
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -180,7 +181,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
