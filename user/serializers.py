b6457c9da129a65bf38f399da3f8c75bd0b1507a
YouweiPeng <98921666+YouweiPeng@users.noreply.github.com>
2024-10-31 17:34:29 -0600

    fix room_number edit

--- user/serializers.py
+++ user/serializers.py
@@ -4,4 +4,4 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['uuid', 'username', "phone_number", "email", "address"]
+        fields = ['uuid', 'username', "phone_number", "email", "address", 'room_number']
